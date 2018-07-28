a1dc06783ee02821800eb6276898558953980563
Renato Cassino <renatocassino@gmail.com>
2018-07-28 19:56:41 -0300

    Environment variables

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -3,7 +3,7 @@ defmodule Facerkutv2.UserController do
 
   alias Facerkutv2.{Repo, Community, User, UserSerializer}
   def login(conn, _params) do
-    secret = System.get_env("JWT_TOKEN") || "123456789abcdefghijklmnopqrstuvwxyz"
+    secret = System.get_env("JWT_SECRET_KEY") || "123456789abcdefghijklmnopqrstuvwxyz"
     jwt = Repo.get_by(User, %{email: "renatocassino@gmail.com"})
     |> UserSerializer.serializer()
     |> JsonWebToken.sign(%{key: secret})
